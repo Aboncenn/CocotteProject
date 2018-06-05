ddec5dc5cd36e7cd6816de0799c2b35ecf507178
AnthonyBarei <anthony.barei@outlook.fr>
2018-06-05 16:03:40 +0200

    vue.js

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Chicken</title>
+    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script> <!-- CDN Vue.js -->
+  </head>
+  <body>
+
+    <div id="app">
+        {{ message }}
+    </div>
+
+    <script type="text/javascript" src="app.js"></script>
+
+  </body>
+</html>
