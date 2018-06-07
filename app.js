1264733877b5008b16306480138222e3e7d1153b
Anthony Barei <anthony.barei@outlook.fr>
2018-06-07 15:51:12 +0200

    working puppets list

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
-var express = require('express')
-var cors = require('cors')
-var app = express()
-
-app.use(cors({
-  origin: 'http://localhost:8888',
-  credentials: true
-}));
-
 const vm = new Vue({
-  el: '#app',
-  data: {
-    puppets: []
-  },
-  mounted() {
-    // axios.get("http://localhost:3000/puppets")
-    // .then(response => {this.puppets = response.data.results});
+    el: '#app',
+    data: {
+        puppets: [],
+        puppet_form_name: null,
+        puppet_form_btMac: null,
+        puppet_form_btPw: null
+    },
+    mounted() {
+        axios.get("http://localhost:3000/puppets", { headers : { "Access-Control-Allow-Origin" : "*" } }).then((response) => {
+            console.log(response.data)
+        })
 
-    axios.get("http://localhost:3000/puppets", { headers : { "Access-Control-Allow-Origin" : "*" } }).then((response) => {
-      console.log(response.data)
-    })
-  }
+        axios.get("http://localhost:3000/puppets", { headers : { "Access-Control-Allow-Origin" : "*" } }).then((response) => {
+            this.puppets = response.data
+        })
+    },
+    methods: {
+        formValues: function (puppet) {
+            // `this` fait référence à l'instance de Vue à l'intérieur de `methods`
+            //$('#name').val(puppet.name);
+            this.puppet_form_name = puppet.name;
+            this.puppet_form_btMac = puppet.btMac;
+            this.puppet_form_btPw = puppet.btPw;
+        }
+    }
 });
