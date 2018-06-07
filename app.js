2fbcf889102999ef42129a42bbaa11f3ca489193
Anthony Barei <anthony.barei@outlook.fr>
2018-06-07 14:30:04 +0200

    vue js tests

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
-var app = new Vue({
+var express = require('express')
+var cors = require('cors')
+var app = express()
+
+app.use(cors({
+  origin: 'http://localhost:8888',
+  credentials: true
+}));
+
+const vm = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+    puppets: []
+  },
+  mounted() {
+    // axios.get("http://localhost:3000/puppets")
+    // .then(response => {this.puppets = response.data.results});
+
+    axios.get("http://localhost:3000/puppets", { headers : { "Access-Control-Allow-Origin" : "*" } }).then((response) => {
+      console.log(response.data)
+    })
   }
 });
